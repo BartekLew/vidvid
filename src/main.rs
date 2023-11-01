07bbf36ca4734e198e52e145f676994a745fdc85
Lew <lew@wiedz.net.pl>
2023-11-01 21:09:17 +0100

    multiple matching in Matcher

--- src/main.rs
+++ src/main.rs
@@ -32,10 +32,35 @@ impl Matchable for u64 {
     }
 }
 
+#[cfg(test)]
+struct MatcherLoop<'a, R, F>
+where
+    F: Fn(Matcher<()>) -> Matcher<R>,
+{
+    tail: Option<&'a str>,
+    f: F,
+}
+
+#[cfg(test)]
+impl<'a, R, F> Iterator for MatcherLoop<'a, R, F>
+where
+    F: Fn(Matcher<()>) -> Matcher<R>,
+{
+    type Item = R;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let tail = self.tail.take();
+        let m = (self.f)(matcher(tail?));
+        self.tail = Some(m.tail);
+        m.val
+    }
+}
+
 struct Matcher<'a,T> {
     tail: &'a str,
     val: Option<T>
 }
+
 
 impl<'a,T> Matcher <'a,T> {
     fn then<F, R> (self, f: F) -> Matcher<'a, R>
@@ -43,6 +68,74 @@ impl<'a,T> Matcher <'a,T> {
         match self.val {
             Some(v) => f(self.tail, v),
             None => Matcher { tail: self.tail, val: None }
+        }
+    }
+
+    #[cfg(test)]
+    fn after(mut self, refs: &'a str) -> Self {
+        let mut finder = matcher(self.tail);
+        while finder.tail.len() > refs.len() {
+            finder = finder.const_str(refs);
+            if finder.val.is_some() {
+                self.tail = finder.tail;
+                return self;
+            }
+            finder.tail = &finder.tail[1..];
+            finder.val = Some(());
+        }
+
+        self.val = None;
+        return self;
+    }
+
+    #[cfg(test)]
+    fn until_word(self) -> Matcher<'a, &'a str> {
+        let mut cur = self.tail;
+        let mut ans;
+        while cur.len() > 0 {
+            ans = matcher(cur).word();
+            if ans.val.is_some() {
+                return ans;
+            }
+            cur = &cur[1..];
+        }
+
+        Matcher { tail: self.tail, val: None }
+    }
+
+    #[cfg(test)]
+    fn until<R: Matchable>(self) -> Matcher<'a, <R as Matchable>::Out> {
+        let mut cur = self.tail;
+        let mut ans;
+        while cur.len() > 0 {
+            ans = matcher(cur).value::<R>();
+            if ans.val.is_some() {
+                return ans;
+            }
+            cur = &cur[1..];
+        }
+
+        Matcher { tail: self.tail, val: None }
+    }
+
+    #[cfg(test)]
+    fn many<R,F>(self, f:F) -> MatcherLoop<'a,R,F>
+            where F: Fn(Matcher<()>) -> Matcher<R> {
+        MatcherLoop { tail: Some(self.tail), f }
+    }
+
+    #[cfg(test)]
+    fn word(self) -> Matcher<'a, &'a str> {
+        let mut i: usize = 0;
+        let mut cur = self.tail.chars();
+        while cur.next().map(|c| c.is_alphabetic()).unwrap_or(false) {
+            i += 1;
+        }
+
+        if i > 0 {
+            Matcher { tail: &self.tail[i..], val: Some(&self.tail[0..i]) }
+        } else {
+            Matcher { tail: self.tail, val: None }
         }
     }
 
@@ -72,9 +165,9 @@ impl<'a,T> Matcher <'a,T> {
         })
     }
 
-    fn merge<R, F> (self, f: F) -> Matcher<'a, <R as Matchable>::Out>
+    fn merge<R, V, F> (self, f: F) -> Matcher<'a, V>
             where R: Matchable,
-                  F: Fn(T, <R as Matchable>::Out) -> <R as Matchable>::Out {
+                  F: Fn(T, <R as Matchable>::Out) -> V {
         self.then(|tail, v1| {
             match R::scan(tail) {
                 Some((t2, v2)) => Matcher { tail: t2, val: Some(f(v1, v2)) },
@@ -108,7 +201,7 @@ impl Write for TimeCtl {
                 match matcher(s).const_str("A: ")
                                 .value::<u64>()
                                 .const_str(".")
-                                .merge::<u64, _>(|units, frac| units*10 + frac)
+                                .merge::<u64, u64, _>(|units, frac| units*10 + frac)
                                 .result() {
                     Some(x) => { self.time = x },
                     None => {}
@@ -167,7 +260,7 @@ fn main() {
 
 #[cfg(test)]
 mod test {
-    use crate::matcher;
+    use crate::*;
 
     #[test]
     fn matcher_matches_number () {
@@ -201,7 +294,7 @@ mod test {
                     .const_str("= ")
                     .value::<u64>()
                     .const_str(" + ")
-                    .merge::<u64, _>(|a, b| a + b)
+                    .merge::<u64, u64, _>(|a, b| a + b)
                     .result();
         assert_eq!(m, Some(7));
     }
@@ -215,10 +308,63 @@ mod test {
         let m2 = m.const_str(" / ");
         assert_eq!(m2.result_ref(), &Some(4));
 
-        let m3 = m2.merge::<u64, _>(|a, b| a / b)
+        let m3 = m2.merge::<u64, u64,_>(|a, b| a / b)
                          .result();
 
         assert_eq!(m3, None);
+    }
+
+    #[test]
+    fn matcher_matches_after () {
+        let m = matcher("foo bar baz 43 11")
+                    .after(" baz ")
+                    .value::<u64>()
+                    .result();
+
+        assert_eq!(m, Some(43));
+    }
+
+    #[test]
+    fn matcher_matches_words () {
+        let m = matcher("fas; ca")
+                    .word()
+                    .result();
+        assert_eq!(m, Some("fas"));
+    }
+
+    #[test]
+    fn matcher_searches_word() {
+        let m = matcher(">> foo = 32 baz")
+                     .until_word()
+                     .const_str(" = ")
+                     .merge::<u64, (&str, u64), _>(|name, val| (name,val))
+                     .result();
+        assert_eq!(m, Some(("foo", 32)));
+    }
+
+    #[test]
+    fn matcher_matches_many_numbers() {
+        let mut it = matcher("43 31 533")
+                        .many(|m1| m1.until::<u64>());
+
+        assert_eq!(it.next(), Some(43));
+        assert_eq!(it.next(), Some(31));
+        assert_eq!(it.next(), Some(533));
+        assert_eq!(it.next(), None);
+    }
+
+    #[test]
+    fn matcher_matches_many () {
+        let mut it = matcher("foo=1 bar=2 zza=3")
+                       .many(|m1|
+                             m1.until_word()
+                               .const_str("=")
+                               .merge::<u64,(String, u64),_>(|name, val| (name.to_owned(),val)));
+
+        assert_eq!(it.next(), Some(("foo".to_owned(),1)));
+        assert_eq!(it.next(), Some(("bar".to_owned(),2)));
+        assert_eq!(it.next(), Some(("zza".to_owned(),3)));
+        assert_eq!(it.next(), None);
     }
 }
 
